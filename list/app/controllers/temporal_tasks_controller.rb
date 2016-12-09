0e365fa4718509b2a658f583b586240e69edf2a7
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2016-12-09 20:59:32 -0300

    empezando los temporal tasks..

--- list/app/controllers/temporal_tasks_controller.rb
+++ list/app/controllers/temporal_tasks_controller.rb
@@ -1,2 +1,56 @@
 class TemporalTasksController < ApplicationController
+	before_action :findparent
+	before_action :findtask, only: [:update, :edit]
+	
+	def new
+		@temporalTask=TemporalTask.new
+	end
+
+	def edit
+	end
+
+  	def create
+
+	  	@temporalTask=TemporalTask.new(task_params)
+
+		@temporalTask.list_id=@list.id
+
+		if @temporalTask.save
+
+			redirect_to list_path(@list)
+		else
+			render 'new'
+		end
+	end
+
+	def update
+	  if @temporalTask.update(task_params)
+	    redirect_to @list
+	  else
+	    render 'edit'
+	  end
+	end
+	
+
+	def destroy
+	    @task = @list.tasks.find(params[:id])
+	    @task.destroy
+	    redirect_to list_path(@list)
+	end
+
+
+  private
+
+  def findtask
+  	@temporalTask=TemporalTask.find(params[:id])
+  end
+
+  def findparent
+	  url=params[:list_id]
+	  @list = List.find_by(url: "#{url}")
+	end
+
+  def task_params
+  	params.require(:temporal_task).permit(:description, :state, :priority)
+	end
 end
